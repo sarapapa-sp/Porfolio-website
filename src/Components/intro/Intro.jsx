5644bd1c037b6c45f4846cdd00de2701103d80e4
Anurag12-Patidar <patidar.rakesh@s.amity.edu>
2022-09-01 10:59:41 +0530

    added the left side of intro section

---
+++ src/Components/intro/Intro.jsx
@@ -0,0 +1,42 @@
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div className='intro'>
+        <div className="intro-left">
+            <div className="left-wrapper">
+                <h2 className="i-intro">Hello , my name is </h2>
+                <h1 className="i-name">Shubham Pawar</h1>
+                <div className="i-title">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">
+                            Backend Developer
+                        </div>
+                        <div className="i-title-item">
+                            Full stack Developer
+                        </div>
+                        <div className="i-title-item">
+                            Artificial intelligence
+                        </div>
+                        <div className="i-title-item">
+                            Cyber Security
+                        </div>
+                        <div className="i-title-item">
+                            Automation
+                        </div>
+
+                    </div>
+                </div>
+                <div className="i-description">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse voluptatibus dolorum dolore, dolorem qui totam nemo voluptate, enim praesentium consequatur eaque nam aspernatur modi repudiandae libero culpa, accusantium harum quam!
+                </div>
+            </div>
+        </div>
+        <div className="intro-right">
+            <div className="right-wrapper"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Intro
