8e6d7d2007ee7743bc31fe07502ff50a0f9e8267
sarapapa-sp <shubhampawar1705@gmail.com>
2022-09-28 18:55:52 +0530

    added the About us component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from "./Components/intro/Intro";
+import About from "./Components/about/About";
 
 
 function App() {
   return (
     <div className="App">
       <Intro />
+        <About />
     </div>
   );
 }
