f3191b3e8bfec1a244691495053def8a2c7c9270
sarapapa-sp <shubhampawar1705@gmail.com>
2022-10-05 11:30:38 +0530

    changes in node modules , added the IPL dashboard to projects

--- src/data.js
+++ src/data.js
@@ -3,20 +3,29 @@ export const products = [
         id:1,
         img:"https://i.ibb.co/7QLx8df/hotelbooking.png",
         link:"https://github.com/sarapapa-sp/Booking_App",
+        name:"Booking App",
+        info:"Hotel booking web app using MERN stack",
     },
     {
         id:2,
-        img:"https://i.ibb.co/kcSnQLc/hotel2.png",
-        link:"https://github.com/sarapapa-sp/Booking_App",
+        img:"https://i.ibb.co/GCdnpnr/mumbai-indians-profile.png",
+        link:"https://github.com/sarapapa-sp/IPL-dashboard",
+        name:"IPL dashboard",
+        info:"IPL dashboard for all the teams from 2008-2018, using Spring Boot and ReactJs",
     },
     {
         id:3,
         img:"https://i.ibb.co/kcSnQLc/hotel2.png",
         link:"https://github.com/sarapapa-sp/Booking_App",
+        name:"",
+        info:"",
     },
     {
         id:4,
         img:"https://i.ibb.co/kcSnQLc/hotel2.png",
         link:"https://github.com/sarapapa-sp/Booking_App",
+        name:"",
+        info:"",
+
     },
 ]
